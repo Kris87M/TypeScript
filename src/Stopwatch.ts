3307a441eaccca18a88eff7bc040e1eb01ff9276
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-01-10 00:05:02 +0100

    Add DOM elements and event listeners for stopwatch controls

--- src/Stopwatch.ts
+++ src/Stopwatch.ts
@@ -1,31 +1,36 @@
+interface StopwatchDom {
+currentTime: HTMLDivElement;
+startBtn: HTMLButtonElement;
+stopBtn: HTMLButtonElement;
+resetBtn: HTMLButtonElement;
+[x: string]: HTMLElement
+}
+
 abstract class Stopwatch {
 
-  currentTime = 0
-  timer = null
-  dom = {}
+  protected currentTime: number = 0
+  private timer: number | null = null
+  protected dom = {} as StopwatchDom;
 
-  constructor(element) {
+  constructor(element: HTMLDivElement) {
     this.getElements(element)
     this.initActions()
     this.renderTime()
   }
 
-  getElements(element) {
-    /*
-    Funkcja ta powinna przyjąć jako argument referencję do elementu DOM, w którym znajduje się
-    cała struktura stopera. Następnie powinna przygotować referencję do obecnych w tym elemencie dzieci.
-    
-    Konkretnie do: 
-    – stopwatch__current-time, 
-    – wszystkich buttonów stopwatch__actions
-
-    Wszystkie referencje dla czytelności przechowuj w obiekcie this.dom.
-    */
+  private getElements(element: HTMLDivElement): void {
+    this.dom.currentTime = element.querySelector('.stopwatch__current-time') as HTMLDivElement;
+    this.dom.startBtn = element.querySelector('.stopwatch__start-btn') as HTMLButtonElement;
+    this.dom.stopBtn = element.querySelector('.stopwatch__stop-btn') as HTMLButtonElement;
+    this.dom.resetBtn = element.querySelector('.stopwatch__reset-btn') as HTMLButtonElement;
   }
 
-  initActions() {
+  initActions(): void {
+    this.dom.startBtn.addEventListener('click', () => this.start());
+    this.dom.stopBtn.addEventListener('click', () => this.stop());
+    this.dom.resetBtn.addEventListener('click', () => this.reset());
     /*
-    Funkcja ta powinna nadać buttonom z buttonów stopwatch__actions odpowiednie nasłuchiwacze na event click. 
+    Funkcja ta powinna nadać buttonom z buttonów stopwatch__actions odpowiednie nasłuchiwacze na event click.
     Kliknięcie na każdy z buttonów powinno uruchamiać odpowiednie funkcje.
 
     Start -> start()
@@ -40,16 +45,17 @@ abstract class Stopwatch {
     /*
     Funkcja ta powinna przyjmować czas w milisekundach a następnie zwracać go w formacie mm:ss:ms (np. 02:23:12).
     */
-  } 
+  }
 
   renderTime() {
     /*
     Funkcja ta powinna renderować w stopwatch__current-time zawartość obiektu this.currentTime.
-    Oczywiście wcześniej należy sformatować czas przy użyciu funkcji this.formatTime. 
+    Oczywiście wcześniej należy sformatować czas przy użyciu funkcji this.formatTime.
     */
   }
 
   start() {
+    console.log('start')
     /*
     Funkcja ta powinna wystartować interwał, który będzie wykonywał się co milisekundę.
     Powinien on każdorazowo włączać funkcję this.step
@@ -66,12 +72,14 @@ abstract class Stopwatch {
   }
 
   stop() {
-    /* 
+    console.log('stop')
+    /*
     Funkcja ta powinna zatrzymywać interval przypisany do this.timer.
     */
   }
 
   reset() {
+    console.log('reset')
     /*
     Ta funkcja powinna resetować czas zapisany w this.currentTime, a więć zmieniać jego wartość na zero.
     Naturalnie powinno to wiązać się również z przerenderowaniem HTML-a (this.renderTime).
