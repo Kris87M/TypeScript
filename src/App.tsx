042dcc1843b59a7b72f663d9000f069e6dac9268
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-01-13 16:10:29 +0100

    Init task

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FC, useState } from 'react';
+
+interface Book {
+  id: string;
+  title: string;
+  author: string;
+  price: number;
+}
+
+const booksData: Book[] = [
+  { id: 'te1314derw', title: 'Of Mice and Men', author: 'John Steinbeck', price: 25 },
+  { id: 't32s14dzdw', title: 'East of Eden', author: 'John Steinbeck', price: 30 }
+];
+
+const App: FC = () => {
+  const [books, setBooks] = useState<Book[]>(booksData);
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Books App</h1>
+      </header>
+      {books.map((book: Book) => <span>{book.title}</span>)}
+    </div>
+  );
+}
+
+export default App;
