37a87eea9ee0a8318844c591d6bcd8d013dcc66d
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-01-13 18:36:36 +0100

    Add components and styles (BooksList and BookItem)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { FC, useState } from 'react';
-
-interface Book {
-  id: string;
-  title: string;
-  author: string;
-  price: number;
-}
+import BooksList from './components/BooksList/BooksList';
+import { Book } from './interfaces/Book.interface';
 
 const booksData: Book[] = [
   { id: 'te1314derw', title: 'Of Mice and Men', author: 'John Steinbeck', price: 25 },
@@ -21,7 +16,7 @@ const App: FC = () => {
       <header>
         <h1>Books App</h1>
       </header>
-      {books.map((book: Book) => <span>{book.title}</span>)}
+      <BooksList books={books} />
     </div>
   );
 }
